f2f2dac270b73fbddedea2c003bb5eab17738c50
lizhenghn123 <lizhenghn@gmail.com>
2015-01-05 21:11:46 +0800

    add daemon tools, see https://github.com/cloudwu/skynet/blob/master/skynet-src/skynet_daemon.c

---
+++ docs/README.md
@@ -0,0 +1,6 @@
+
+
+* [Schmidt的三篇reactor文献]
+	* [Reactor ： An Object Behavioral Pattern for Demultiplexing and Dispatching Handles for Synchronous Events](http://www.cs.wustl.edu/~schmidt/PDF/reactor-siemens.pdf)
+	* [The Reactor : An Object-Oriented Wrapper for Event-Driven Port Monitoring and Service Demultiplexing](https://www.dre.vanderbilt.edu/~schmidt/PDF/Reactor1-93.pdf)
+	* [The Reactor 2 : The Design and Implementation of the Reactor An Object-Oriented Framework for Event Demultiplexing](https://www.dre.vanderbilt.edu/~schmidt/PDF/Reactor2-93.pdf)
---
+++ zlreactor/base/Daemonize.h
@@ -0,0 +1,25 @@
+﻿// ***********************************************************************
+// Filename         : Daemonize.h
+// Author           : LIZHENG
+// Created          : 2015-01-05
+// Description      : 
+//
+// Last Modified By : LIZHENG
+// Last Modified On : 2015-01-05
+//
+// Copyright (c) lizhenghn@gmail.com. All rights reserved.
+// ***********************************************************************
+#ifndef ZL_DAEMONIZE_H
+#define ZL_DAEMONIZE_H
+namespace zl
+{
+namespace base
+{
+
+int createDaemonize(const char *pidfile, int nochdir = 1, int noclose = 0);
+
+int exitDaemonize(const char *pidfile);
+
+}
+}
+#endif  /* ZL_DAEMONIZE_H */
