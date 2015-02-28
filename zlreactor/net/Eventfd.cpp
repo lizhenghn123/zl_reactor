c2166c0ccffbe704ee52d03a979e6d4e78c2cdbd
lizhenghn123 <lizhenghn@gmail.com>
2015-02-28 16:40:34 +0800

    [ADD] Eventfd.h

---
+++ zlreactor/net/Eventfd.cpp
@@ -0,0 +1,46 @@
+﻿#include "net/Eventfd.h"
+#include <sys/eventfd.h>
+#include <unistd.h>
+#include "base/ZLog.h"
+#include "net/SocketUtil.h"
+NAMESPACE_ZL_NET_START
+
+EventfdHandler::EventfdHandler()
+{
+    eventfd_ = -1;
+}
+
+EventfdHandler::~EventfdHandler()
+{
+     if(eventfd_ != -1)
+     {
+         ::close(eventfd_);
+         eventfd_ = -1;
+     }
+}
+
+int EventfdHandler::createEventfd()
+{
+    int efd = ::eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
+    if (efd < 0)
+    {
+        LOG_ALERT("create eventfd failed in EventfdHandler::createEventfd()");
+        return efd;
+    }
+    LOG_INFO("EventfdHandler::createEventfd [%d]", efd);
+
+    eventfd_ = efd;
+    return efd;
+}
+
+ssize_t EventfdHandler::write(const void *data, size_t len)
+{
+    return SocketUtil::write(eventfd_, data, len);
+}
+
+ssize_t EventfdHandler::read(void *buf, size_t size)
+{
+	return SocketUtil::read(eventfd_, buf, size);
+}
+
+NAMESPACE_ZL_NET_END
---
+++ zlreactor/net/Eventfd.h
@@ -0,0 +1,38 @@
+﻿// ***********************************************************************
+// Filename         : Eventfd.h
+// Author           : LIZHENG
+// Created          : 2015-01-14
+// Description      : eventfd need linux kernel > 2.6.22
+//
+// Last Modified By : LIZHENG
+// Last Modified On : 2015-01-14
+//
+// Copyright (c) lizhenghn@gmail.com. All rights reserved.
+// ***********************************************************************
+#ifndef ZL_EVENTFD_H
+#define ZL_EVENTFD_H
+#include "Define.h"
+NAMESPACE_ZL_NET_START
+
+class EventfdHandler
+{
+public:
+    EventfdHandler();
+    ~EventfdHandler();
+
+public:
+    /// iff success, return eventfd_, else return -1;
+    int createEventfd();
+
+    int eventfd() { return eventfd_; }
+
+    ssize_t write(const void *data, size_t len);
+
+    ssize_t read(void *buf, size_t size);
+
+private:
+    int   eventfd_;
+};
+
+NAMESPACE_ZL_NET_END
+#endif  /* ZL_EVENTFD_H */
