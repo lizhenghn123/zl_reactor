31988b432c7fcb36255d67074d624fb448bdb5a9
lizheng <lizhenghn@gmail.com>
2015-06-01 22:11:22 +0800

    重要：更改Atomic的实现

--- zlreactor/thread/tests/Atomic_test.cpp
+++ zlreactor/thread/tests/Atomic_test.cpp
@@ -9,12 +9,12 @@ int main()
 		zl::thread::Atomic<int> ato;
 		ato++;
 		++ato;
-		ato.fetchAndInc(10);
+		ato.getAndAdd(10);
 		ato += 100;
 		ato -= 3;
 		std::cout << ato.value() << "\n";
 
-		long t = ato.decAndFetch(3);
+		long t = ato.subAndGet(3);
 		std::cout << ato << "\t" << t << "\n";
 	}
 	{
@@ -22,18 +22,11 @@ int main()
 		//zl::thread::Atomic<int> ato;
         zl::thread::AtomicInt32 ato;
 
-		std::cout << ato.fetchAndInc(2) << "\t" ;
+		std::cout << ato.getAndAdd(2) << "\t" ;
 		std::cout << ato.value() << "\n";
 
-		std::cout << ato.fetchAndDec(3) << "\t" ;
+		std::cout << ato.addAndGet(3) << "\t" ;
 		std::cout << ato.value() << "\n";
-	}
-	{
-		std::cout << "========================================\n";
-		zl::thread::Atomic<int> ato;
-
-		std::cout << ato.incAndFetch(2) << "\t" ;
-		std::cout << ato << "\n";
 	}
     {
         std::cout << "========================================\n";
