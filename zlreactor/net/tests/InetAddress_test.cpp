7a120bfbc9123cb5901bb020a44cdc378263d698
lizheng <lizhenghn@gmail.com>
2015-07-07 19:56:06 +0800

    InetAddress_test.cpp

---
+++ zlreactor/net/tests/InetAddress_test.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <assert.h>
+#include <string>
+#include "net/InetAddress.h"
+#include "net/Socket.h"
+#include "net/SocketUtil.h"
+using namespace std;
+using namespace zl;
+using namespace zl::net;
+
+void test_InetAddress()
+{
+    {
+        InetAddress addr(8888);
+        assert(addr.port()== 8888);
+        assert(addr.ip() == string("0.0.0.0"));
+        assert(addr.ipPort() == string("0.0.0.0:8888"));
+    }
+    {
+        InetAddress addr("192.168.14.7", 8888);
+        assert(addr.ip() == string("192.168.14.7"));
+        assert(addr.ipPort() == string("192.168.14.7:8888"));
+    }
+    {
+        InetAddress addr("192.168.14.7", 8888);
+        Socket socket(SocketUtil::createSocket());
+        assert(socket.fd() > 0);
+        bool ret = socket.bind(addr);
+        assert(ret);
+        ret = socket.listen(1024);
+        assert(ret);
+        cout << "server listening on " << addr.ipPort() << ", waiting for incoming....\n";
+        while(true)
+        {
+            InetAddress clientAddr;
+            ZL_SOCKET clientFd = socket.accept(&clientAddr);    // test: nc 192.168.14.7 8888 
+            cout << "accept one client from " << clientAddr.ipPort() << "\n";
+            SocketUtil::closeSocket(clientFd);
+        }
+    }
+}
+
+int main()
+{
+	test_InetAddress();
+    printf("###### GAME OVER ######");
+}
+
